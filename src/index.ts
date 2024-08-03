cdd681a1fee3ede162ef45269a1685c79ba5392c
Jake Mingolla <jake.mingolla@robinpowered.com>
2024-08-03 17:09:53 -0400

    Init/v2 (#2)
    * checkpoint
    
    * rest of files
    
    * rename
    
    * support up to 30
    
    * lock types
    
    * freeze lockfile in CI
    
    * fix imports
    
    * add bun types
    
    * added jest types
    
    * remove freeze

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-console.log("Hello via Bun!");
-const df = "f";
-void df;
+import type { PositiveInteger } from "./types/positive-integer";
+
+const CITYS_BLESSING = 10;
+
+const createTokensFromOcelotPrides = (
+  remainingOcelots: PositiveInteger,
+  createdCats: PositiveInteger,
+  startingPermaments: PositiveInteger,
+): PositiveInteger => {
+  if (remainingOcelots === 0) {
+    return createdCats;
+  }
+
+  createdCats += 1;
+
+  if (startingPermaments + createdCats >= CITYS_BLESSING) {
+    createdCats += createdCats;
+  }
+
+  return createTokensFromOcelotPrides(
+    remainingOcelots - 1,
+    createdCats,
+    startingPermaments,
+  );
+};
+
+export const calculate = (
+  ocelots: PositiveInteger,
+  guides: PositiveInteger,
+  permanents: PositiveInteger,
+): { catTokens: PositiveInteger; energy: PositiveInteger } => {
+  const catTokens = createTokensFromOcelotPrides(ocelots, 0, permanents);
+  const energy = guides * catTokens;
+
+  return { catTokens, energy };
+};
