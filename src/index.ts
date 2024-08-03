0ec169c8fd9d4bcd927b97676c31442bfd11bf58
jakemingolla <jake.mingolla@robinpowered.com>
2024-08-03 16:50:21 -0400

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-console.log("Hello via Bun!");
-const df = "f";
-void df;
+import type { PositiveInteger } from "./types/positive-integer";
+
+const CITYS_BLESSING = 10;
+
+const foo = (
+  remainingOcelots: PositiveInteger,
+  createdCats: PositiveInteger,
+  startingPermaments: PositiveInteger,
+): PositiveInteger => {
+  if (remainingOcelots === 0) {
+    return createdCats;
+  }
+
+  createdCats += 1;
+
+  if (startingPermaments + createdCats >= CITYS_BLESSING) {
+    createdCats += createdCats;
+  }
+
+  return foo(remainingOcelots - 1, createdCats, startingPermaments);
+};
+
+export const calculate = (
+  ocelots: PositiveInteger,
+  guides: PositiveInteger,
+  permanents: PositiveInteger,
+): { catTokens: PositiveInteger; energy: PositiveInteger } => {
+  const catTokens = foo(ocelots, 0, permanents);
+  const energy = guides * catTokens;
+
+  return { catTokens, energy };
+};
